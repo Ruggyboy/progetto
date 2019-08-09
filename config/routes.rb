87de539da30323863ad8e9718e355f5d878ab490
FrancescoLY <francescoluro44@gmail.com>
2019-08-09 20:23:07 +0200

    commit09_08

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   resources :users
+  get 'users/new'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
