e6c24dbc095a8db56f227931846d35977f99c4cf
FrancescoLY <francescoluro44@gmail.com>
2019-08-12 20:47:52 +0200

    12_08

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
-  get 'users/new'
+  # get 'users/new'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
