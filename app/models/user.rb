ba8fd6747d3451a71a92bac2e069cbe17d697244
Ingegneria Informatica e Automatica <FrancescoLY>
2019-10-30 21:47:41 +0100

    _

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
     validates :name,  presence: true, uniqueness: true, length: { maximum: 35 }
-    validates :email, presence: true, length: {maximum: 255}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }
 
     attr_accessor :remember_token, :activation_token, :reset_token
 
